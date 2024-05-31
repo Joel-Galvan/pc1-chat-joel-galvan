2ae925ece8f8f90a36b0755fe812839c1a733a9a
Joel <joelgv1003@gmail.com>
2024-05-30 22:43:31 -0500

    boton darkmode sin darkmode

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,13 +1,23 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 
 export default function Header({title}) {
+    const [darkMode, setDarkMode] = useState(false);
+
+  const handleToggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    onToggleDarkMode(!darkMode);
+  };
+
     return (
-        <div className="bg-blue-500 text-white p-4 flex justify-between items-center">
-          <h1 className="text-lg font-bold">{title}</h1>
-          <div>
-            <Link to="/" className="mr-4">cerrar sesion</Link>
-          </div>
-        </div>
-      );
+        <div className={`bg-${darkMode ? 'gray-800' : 'blue-500'} text-white p-4 flex justify-between items-center`}>
+      <h1 className="text-lg font-bold">{title}</h1>
+      <div>
+        <button onClick={handleToggleDarkMode} className="mr-4">Dark Mode</button>
+        <Link to="/" className="mr-4">Cerrar Sesion</Link>
+
+      </div>
+    </div>
+  );
 }
